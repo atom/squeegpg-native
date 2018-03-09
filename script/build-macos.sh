2218afcd19dd2d61cadd75a1b86e531e6c74ba3c
Ash Wilson <smashwilson@gmail.com>
2018-03-08 23:11:00 -0500

    Refactor out common module building

--- script/build-macos.sh
+++ script/build-macos.sh
@@ -15,204 +15,56 @@ source "${SCRIPT}/helper/prereq.sh"
 # shellcheck source=helper/title.sh
 source "${SCRIPT}/helper/title.sh"
 
-## Verify build prerequisites #########################################################################################
-
-has automake
+# shellcheck source=helper/module.sh
+source "${SCRIPT}/helper/module.sh"
 
 ## Download and build prerequisites ###################################################################################
 
-function getversion {
-  local DEP=$1
+module -n libgpg-error \
+  -c enable-static -c disable-shared -c disable-rpath \
+  -c disable-doc -c disable-tests -c disable-languages
 
-  sed -n -e "s/^${DEP}:[[:space:]]*\([[:digit:]][[:digit:].]*\)/\\1/p" "${ROOT}/versions"
-}
+module -n npth \
+  -c enable-static -c disable-shared -c disable-tests
 
-function depurl {
-  local DEP=$1
+module -n libgcrypt \
+  -c enable-static -c disable-shared \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error \
+  -c with-pth-prefix=${ROOT}/build/deps/npth
 
-  printf "https://gnupg.org/ftp/gcrypt/${DEP}/${DEP}-$(getversion ${DEP}).tar.bz2"
-}
+module -n libassuan \
+  -c enable-static -c disable-shared -c disable-doc \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error
 
-function depargs {
-  local DEP=$1
+module -n libksba \
+  -c enable-static -c disable-shared \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error
 
-  case "${DEP}" in
-    libgpg-error)
-      echo -n \
-        "--enable-static --disable-shared --disable-rpath " \
-        "--disable-doc --disable-tests --disable-languages"
-      ;;
-    npth)
-      echo -n \
-        "--enable-static --disable-shared " \
-        "--disable-tests"
-      ;;
-    libgcrypt)
-      echo -n \
-        "--enable-static --disable-shared " \
-        "--with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error " \
-        "--with-pth-prefix=${ROOT}/build/deps/npth "
-      ;;
-    libassuan)
-      echo -n \
-        "--enable-static --disable-shared " \
-        "--with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error " \
-        "--disable-doc"
-      ;;
-    libksba)
-      echo -n \
-        "--enable-static --disable-shared " \
-        "--with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error"
-      ;;
-    pinentry)
-      echo -n \
-        "--with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error " \
-        "--with-libassuan-prefix=${ROOT}/build/deps/libassuan"
-      ;;
-    *)
-      ;;
-  esac
-}
-
-for DEP in libgpg-error npth libgcrypt libassuan libksba pinentry; do
-  DEPVERSION="$(getversion ${DEP})"
-  if [ -z "${DEPVERSION}" ]; then
-    error "Unable to find the version of ${DEP}."
-    error "Please add a line like the following to ${ROOT}/versions:"
-    error "  ${DEP}: x.y.z"
-    exit 1
-  fi
-  title "Building ${DEP} ${DEPVERSION}"
-  info "${DEP} version ${DEPVERSION}"
-
-  DEPSRC="${ROOT}/src/${DEP}-${DEPVERSION}"
-  DEPTARBALL="${ROOT}/src/${DEP}-${DEPVERSION}.tar.bz2"
-  DEPTARGET="${ROOT}/build/deps/${DEP}"
-  MARKFILE="${DEPTARGET}/.success"
-
-  mkdir -p ${ROOT}/src/
-
-  if [ -d "${DEPSRC}" ] || [ -f "${DEPTARBALL}" ]; then
-    verbose "Dependency ${DEP} has already been downloaded."
-  else
-    info "Downloading tarball for ${DEP}."
-    curl --silent --fail --output "${DEPTARBALL}" "$(depurl ${DEP})" || {
-      error "Unable to download ${DEP} from URL:"
-      error "$(depurl ${DEP})"
-      exit 1
-    }
-  fi
-
-  if [ -d "${DEPSRC}" ]; then
-    verbose "Dependency ${DEP} has already been unpacked."
-  else
-    info "Unpacking tarball for ${DEPSRC}."
-    cd "${ROOT}/src"
-    tar xjf "${DEPTARBALL}"
-  fi
-
-  if [ -f "${MARKFILE}" ]; then
-    verbose "Dependency ${DEP} has already been built."
-    continue
-  fi
-
-  IFS=" " read -r -a CONFIGURE_ARGS <<< "$(depargs ${DEP})"
-
-  mkdir -p "${DEPTARGET}"
-
-  cd "${DEPSRC}"
-  info "Building dependency ${DEP}."
-  verbose "configure"
-  sh ./configure --prefix="${DEPTARGET}" "${CONFIGURE_ARGS[@]-}" \
-    --disable-dependency-tracking
-  verbose "make"
-  make
-  verbose "make install"
-  make install
-  info "Successfully build ${DEP}."
-  touch "${MARKFILE}"
-done
+module -n pinentry \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error \
+  -c with-libassuan-prefix=${ROOT}/build/deps/libassuan
 
 ## Download and build GPG #############################################################################################
 
-GPGVERSION=$(getversion gnupg)
-if [ -z "${GPGVERSION}" ]; then
-  error "Unable to find the version of GPG to build."
-  error "Please add a line like the following to ${ROOT}/versions:"
-  error "  gnupg: x.y.z"
-  exit 1
-fi
-
-title "Building gpg ${GPGVERSION}"
-info "GPG version ${GPGVERSION}"
-
-GPGSRC="${ROOT}/src/gnupg-${GPGVERSION}"
-GPGURL="https://gnupg.org/ftp/gcrypt/gnupg/gnupg-${GPGVERSION}.tar.bz2"
-GPGTARBALL="${ROOT}/src/gnupg-${GPGVERSION}.tar.bz2"
-GPGTARGET="${ROOT}/build/gnupg"
-MARKFILE="${GPGTARGET}/.success"
-
-if [ -d "${GPGSRC}" ] || [ -f "${GPGTARBALL}" ]; then
-  verbose "GPG has already been downloaded"
-else
-  info "Downloading GPG tarball."
-  curl --silent --fail --output "${GPGTARBALL}" "${GPGURL}" || {
-    error "Unable to download GPG from URL:"
-    error "${GPGURL}"
-    exit 1
-  }
-fi
-
-if [ -d "${GPGSRC}" ]; then
-  verbose "GPG has already been unpacked."
-else
-  info "Unpacking tarball for GPG."
-  cd "${ROOT}/src"
-  tar xjf "${GPGTARBALL}"
-fi
-
-if [ -f "${MARKFILE}" ]; then
-  verbose "GPG has already been built."
-else
-  mkdir -p "${GPGTARGET}"
-
-  cd "${GPGSRC}"
-  info "Building GPG."
-
-  verbose "configure"
-
-  # Workaround for missing libgcrypt include
-  export CFLAGS
-  CFLAGS=$(${ROOT}/build/deps/libgcrypt/bin/libgcrypt-config --cflags)
-
-  sh ./configure --prefix="${GPGTARGET}" \
-    --with-libgpg-error-prefix="${ROOT}/build/deps/libgpg-error" \
-    --with-libgcrypt-prefix="${ROOT}/build/deps/libgcrypt" \
-    --with-libassuan-prefix="${ROOT}/build/deps/libassuan" \
-    --with-ksba-prefix="${ROOT}/build/deps/libksba" \
-    --with-npth-prefix="${ROOT}/build/deps/npth" \
-    --disable-dependency-tracking \
-    --disable-gpgsm \
-    --disable-scdaemon \
-    --disable-dirmngr \
-    --disable-doc \
-    --disable-gpgtar \
-    --disable-photo-viewers \
-    --disable-rpath
-
-  verbose "make"
-  make
-
-  verbose "make install"
-  make install
-
-  info "Successfully build GPG."
-  touch "${MARKFILE}"
-fi
+module -n gnupg \
+  -f "$(${ROOT}/build/deps/libgcrypt/bin/libgcrypt-config --cflags)" \
+  -c disable-gpgsm \
+  -c disable-scdaemon \
+  -c disable-dirmngr \
+  -c disable-doc \
+  -c disable-gpgtar \
+  -c disable-photo-viewers \
+  -c disable-rpath \
+  -c with-libgpg-error-prefix="${ROOT}/build/deps/libgpg-error" \
+  -c with-libgcrypt-prefix="${ROOT}/build/deps/libgcrypt" \
+  -c with-libassuan-prefix="${ROOT}/build/deps/libassuan" \
+  -c with-ksba-prefix="${ROOT}/build/deps/libksba" \
+  -c with-npth-prefix="${ROOT}/build/deps/npth" \
+  -o "${ROOT}/build/gnupg"
 
 info "Dynamic dependencies of the GPG binaries:"
 
-for BINARY in ${GPGTARGET}/bin/*; do
+for BINARY in ${ROOT}/build/gnupg/bin/*; do
   otool -L "${BINARY}"
 done
 
---
+++ script/helper/module.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+function getversion {
+  local MOD=$1
+
+  sed -n -e "s/^${MOD}:[[:space:]]*\([[:digit:]][[:digit:].]*\)/\\1/p" "${ROOT}/versions"
+}
+
+function module {
+  local NAME
+  local VERSION
+  local URL
+  local SRCDIR
+  local TARBALL
+  local OUTDIR=""
+  local CONFIGURE_ARGS=("--disable-dependency-tracking")
+  local EXTRA_CFLAGS=()
+  local MARKFILE
+
+  while getopts ":n:c:f:o:" OPT; do
+    case ${OPT} in
+      n)
+        NAME=${OPT}
+        ;;
+      c)
+        CONFIGURE_ARGS+=("--${OPT}")
+        ;;
+      f)
+        EXTRA_CFLAGS+=("${OPT}")
+        ;;
+      o)
+        OUTDIR="${OPT}"
+        ;;
+      \?)
+        error "module: Invalid option -${OPTARG}."
+        return 1
+        ;;
+      :)
+        error "module: Option -${OPTARG} requires an argument."
+        return 1
+        ;;
+    esac
+
+    if [ -z "${NAME}" ]; then
+      error "module: Option -n is required."
+      return 1
+    fi
+
+    title "Building ${NAME} version ${VERSION}"
+
+    VERSION=$(getversion ${NAME})
+
+    if [ -z "${VERSION}" ]; then
+      error "Unable to find the version of ${NAME}."
+      error "Please add a line like the following to ${ROOT}/versions:"
+      error "  ${NAME}: x.y.z"
+      return 1
+    fi
+
+    URL="https://gnupg.org/ftp/gcrypt/${NAME}/${NAME}-${VERSION}.tar.bz2"
+    SRCDIR="${ROOT}/src/${NAME}-${VERSION}"
+    TARBALL="${ROOT}/src/${NAME}-${VERSION}.tar.bz2"
+    [ -z "${OUTDIR}" ] && OUTDIR="${ROOT}/build/deps/${NAME}"
+    MARKFILE="${OUTDIR}/.success"
+
+    mkdir -p ${ROOT}/src/
+
+    if [ -d "${SRCDIR}" ] || [ -f "${TARBALL}" ]; then
+      verbose "Module ${NAME} has already been downloaded."
+    else
+      info "Downloading tarball for ${NAME}."
+      curl --silent --fail --output "${TARBALL}" "${URL}" || {
+        error "Unable to download ${NAME} from URL:"
+        error "${URL}"
+        exit 1
+      }
+    fi
+
+    if [ -d "${SRCDIR}" ]; then
+      verbose "Module ${NAME} has already been unpacked."
+    else
+      info "Unpacking tarball for ${NAME}."
+      cd "${ROOT}/src" || return 1
+      tar xjf "${TARBALL}"
+    fi
+
+    if [ -f "${MARKFILE}" ]; then
+      verbose "Module ${NAME} has already been built."
+      return 0
+    fi
+
+    mkdir -p "${OUTDIR}"
+    cd "${SRCDIR}" || return 1
+
+    info "Building dependency ${NAME}."
+
+    verbose "configure"
+    export CFLAGS
+    CFLAGS="${EXTRA_CFLAGS[*]-}"
+    sh ./configure --prefix="${OUTDIR}" "${CONFIGURE_ARGS[@]-}"
+
+    verbose "make"
+    make
+
+    verbose "make install"
+    make install
+
+    info "Successfully build ${NAME}."
+    touch "${MARKFILE}"
+  done
+}
