6845b016931a1c541c9a904ea240b8a8696ede9a
Ash Wilson <smashwilson@gmail.com>
2018-03-08 23:24:13 -0500

    Turns out all of that shouldn't be in the loop

--- script/helper/module.sh
+++ script/helper/module.sh
@@ -44,72 +44,73 @@ function module {
         return 1
         ;;
     esac
-
-    if [ -z "${NAME}" ]; then
-      error "module: Option -n is required."
-      return 1
-    fi
-
-    title "Building ${NAME} version ${VERSION}"
-
-    VERSION=$(getversion ${NAME})
-
-    if [ -z "${VERSION}" ]; then
-      error "Unable to find the version of ${NAME}."
-      error "Please add a line like the following to ${ROOT}/versions:"
-      error "  ${NAME}: x.y.z"
-      return 1
-    fi
-
-    URL="https://gnupg.org/ftp/gcrypt/${NAME}/${NAME}-${VERSION}.tar.bz2"
-    SRCDIR="${ROOT}/src/${NAME}-${VERSION}"
-    TARBALL="${ROOT}/src/${NAME}-${VERSION}.tar.bz2"
-    [ -z "${OUTDIR}" ] && OUTDIR="${ROOT}/build/deps/${NAME}"
-    MARKFILE="${OUTDIR}/.success"
-
-    mkdir -p ${ROOT}/src/
-
-    if [ -d "${SRCDIR}" ] || [ -f "${TARBALL}" ]; then
-      verbose "Module ${NAME} has already been downloaded."
-    else
-      info "Downloading tarball for ${NAME}."
-      curl --silent --fail --output "${TARBALL}" "${URL}" || {
-        error "Unable to download ${NAME} from URL:"
-        error "${URL}"
-        exit 1
-      }
-    fi
-
-    if [ -d "${SRCDIR}" ]; then
-      verbose "Module ${NAME} has already been unpacked."
-    else
-      info "Unpacking tarball for ${NAME}."
-      cd "${ROOT}/src" || return 1
-      tar xjf "${TARBALL}"
-    fi
-
-    if [ -f "${MARKFILE}" ]; then
-      verbose "Module ${NAME} has already been built."
-      return 0
-    fi
-
-    mkdir -p "${OUTDIR}"
-    cd "${SRCDIR}" || return 1
-
-    info "Building dependency ${NAME}."
-
-    verbose "configure"
-    export CFLAGS
-    CFLAGS="${EXTRA_CFLAGS[*]-}"
-    sh ./configure --prefix="${OUTDIR}" "${CONFIGURE_ARGS[@]-}"
-
-    verbose "make"
-    make
-
-    verbose "make install"
-    make install
-
-    info "Successfully build ${NAME}."
-    touch "${MARKFILE}"
   done
+
+  if [ -z "${NAME:-}" ]; then
+    error "module: Option -n is required."
+    return 1
+  fi
+
+  info "Building module ${NAME}."
+
+  VERSION=$(getversion ${NAME})
+  if [ -z "${VERSION}" ]; then
+    error "Unable to find the version of ${NAME}."
+    error "Please add a line like the following to ${ROOT}/versions:"
+    error "  ${NAME}: x.y.z"
+    return 1
+  fi
+
+  title "Building ${NAME} version ${VERSION}"
+
+  URL="https://gnupg.org/ftp/gcrypt/${NAME}/${NAME}-${VERSION}.tar.bz2"
+  SRCDIR="${ROOT}/src/${NAME}-${VERSION}"
+  TARBALL="${ROOT}/src/${NAME}-${VERSION}.tar.bz2"
+  [ -z "${OUTDIR}" ] && OUTDIR="${ROOT}/build/deps/${NAME}"
+  MARKFILE="${OUTDIR}/.success"
+
+  mkdir -p ${ROOT}/src/
+
+  if [ -d "${SRCDIR}" ] || [ -f "${TARBALL}" ]; then
+    verbose "Module ${NAME} has already been downloaded."
+  else
+    info "Downloading tarball for ${NAME}."
+    curl --silent --fail --output "${TARBALL}" "${URL}" || {
+      error "Unable to download ${NAME} from URL:"
+      error "${URL}"
+      exit 1
+    }
+  fi
+
+  if [ -d "${SRCDIR}" ]; then
+    verbose "Module ${NAME} has already been unpacked."
+  else
+    info "Unpacking tarball for ${NAME}."
+    cd "${ROOT}/src" || return 1
+    tar xjf "${TARBALL}"
+  fi
+
+  if [ -f "${MARKFILE}" ]; then
+    verbose "Module ${NAME} has already been built."
+    return 0
+  fi
+
+  mkdir -p "${OUTDIR}"
+  cd "${SRCDIR}" || return 1
+
+  info "Building dependency ${NAME}."
+
+  verbose "configure"
+  export CFLAGS
+  CFLAGS="${EXTRA_CFLAGS[*]-}"
+  sh ./configure --prefix="${OUTDIR}" "${CONFIGURE_ARGS[@]-}"
+
+  verbose "make"
+  make
+
+  verbose "make install"
+  make install
+
+  info "Successfully build ${NAME}."
+  touch "${MARKFILE}"
 }
