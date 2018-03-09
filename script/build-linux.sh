f2fff53d180d29e9c49e970607bb52d93c219aba
Ash Wilson <smashwilson@gmail.com>
2018-03-08 23:30:58 -0500

    Build static binaries on Linux

---
+++ script/build-linux.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#
+# Build the current GPG version for Linux.
+
+set -euo pipefail
+
+mkdir -p "${ROOT}/build"
+
+# shellcheck source=helper/log.sh
+source "${SCRIPT}/helper/log.sh"
+
+# shellcheck source=helper/prereq.sh
+source "${SCRIPT}/helper/prereq.sh"
+
+# shellcheck source=helper/title.sh
+source "${SCRIPT}/helper/title.sh"
+
+# shellcheck source=helper/module.sh
+source "${SCRIPT}/helper/module.sh"
+
+## Download and build prerequisites ###################################################################################
+
+module -n libgpg-error \
+  -c enable-static -c disable-shared -c disable-rpath \
+  -c disable-doc -c disable-tests -c disable-languages
+
+module -n npth \
+  -c enable-static -c disable-shared -c disable-tests
+
+module -n libgcrypt \
+  -c enable-static -c disable-shared \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error \
+  -c with-pth-prefix=${ROOT}/build/deps/npth
+
+module -n libassuan \
+  -c enable-static -c disable-shared -c disable-doc \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error
+
+module -n libksba \
+  -c enable-static -c disable-shared \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error
+
+module -n pinentry \
+  -c with-libgpg-error-prefix=${ROOT}/build/deps/libgpg-error \
+  -c with-libassuan-prefix=${ROOT}/build/deps/libassuan
+
+## Download and build GPG #############################################################################################
+
+module -n gnupg \
+  -f "$(${ROOT}/build/deps/libgcrypt/bin/libgcrypt-config --cflags)" \
+  -f "-static" \
+  -c disable-gpgsm \
+  -c disable-scdaemon \
+  -c disable-dirmngr \
+  -c disable-doc \
+  -c disable-gpgtar \
+  -c disable-photo-viewers \
+  -c disable-rpath \
+  -c with-libgpg-error-prefix="${ROOT}/build/deps/libgpg-error" \
+  -c with-libgcrypt-prefix="${ROOT}/build/deps/libgcrypt" \
+  -c with-libassuan-prefix="${ROOT}/build/deps/libassuan" \
+  -c with-ksba-prefix="${ROOT}/build/deps/libksba" \
+  -c with-npth-prefix="${ROOT}/build/deps/npth" \
+  -o "${ROOT}/build/gnupg"
+
+info "Dynamic dependencies of the GPG binaries:"
+
+for BINARY in ${ROOT}/build/gnupg/bin/*; do
+  info "${BINARY}"
+  ldd "${BINARY}" || true
+done
+
+title "✨ Build successful ✨"
